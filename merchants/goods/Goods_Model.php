6bcd26d93708aa68c7a96894f1c637bff5f35f6b
qwe317149766 <772925800@qq.com>
2016-03-23 17:32:07 +0800

    页面bug修改---2016-03-16----hc_edm

--- merchants/goods/Goods_Model.php
+++ merchants/goods/Goods_Model.php
@@ -7,8 +7,16 @@
 defined('IN_SIMPHP') or die('Access Denied');
 
 class Goods_Model extends Model {
-	
-	
+    /**
+     * 获取商家的分类列表
+     */
+    static function getCategoryList(){
+            $merchant_id = $GLOBALS['user']->uid;
+            if(empty($merchant_id)){
+                return false;
+            }
+            $sql="select merchant_id ,cat_name,parent_id ,cat_url from ";
+    }
 }
  
 /*----- END FILE: Goods_Model.php -----*/
