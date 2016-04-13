699bdc7bf596f27ba77c86b81882b377b30f0056
qwe317149766 <772925800@qq.com>
2016-04-13 16:43:14 +0800

    shopController

--- merchants/shop/Shop_Controller.php
+++ merchants/shop/Shop_Controller.php
@@ -6,57 +6,98 @@
  */
 defined('IN_SIMPHP') or die('Access Denied');
 
-class Shop_Controller extends MerchantController {
+class Shop_Controller extends MerchantController
+{
 
-	public function menu()
-	{
-		return [
-			'shop/carousel'=>'carousel_upload'
-		];
-	}
-	/**
-	 * default action 'index'
-	 * @param Request $request
-	 * @param Response $response
-	 */
-	public function index(Request $request, Response $response)
-	{
-		$this->v->set_tplname('mod_shop_index');
-		$this->setSystemNavigate('shop');
-		$this->setPageLeftMenu('shop', 'list');
-		$response->send($this->v);
-	}
+    public function menu()
+    {
+        return [
+            'shop/carousel' => 'carousel_upload',
+            '/shop/carousel/add' => '/shop/carousel_add'
+        ];
+    }
 
-	/**
-	 * 首页轮播图
-	 * @auth edm_hc
-	 * @param Request $request
-	 * @param Response $response
-	 */
-	public function carousel_upload(Request $request, Response $response){
-		$ret = [
-			'flag' => 'FAIL',
-			'errMsg' => '上传失败，请稍后重试！'
-		];
-		if ($request->is_post()) {
-			$imgDIR = "/a/mch/shop/";
-			$img = $_POST["img"];
-			$upload = new Upload($img, $imgDIR);
-			$upload->standardheight = 250;
-			$result = $upload->saveImgData();
-			$ret = $upload->buildUploadResult($result);
-		}
-			$response->sendJSON($ret);
-	}
+    /**
+     * default action 'index'
+     * @param Request $request
+     * @param Response $response
+     */
+    public function index(Request $request, Response $response)
+    {
+        $this->v->set_tplname('mod_shop_index');
+        $this->setSystemNavigate('shop');
+        $this->setPageLeftMenu('shop', 'list');
+        //查出用户所有的轮播图
+        $carousel = Shop_Model::selCarousel();
+        $this->v->assign("carousel", $carousel);
+        $response->send($this->v);
+    }
 
-	/**
-	 * 处理首页轮播图删除
-	 * @param Request $request
-	 * @param Response $response
-	 */
-	public function carousel_del (Request $request, Response $response){
+    /**
+     * 首页轮播图
+     * @auth edm_hc
+     * @param Request $request
+     * @param Response $response
+     */
+    public function carousel_upload(Request $request, Response $response)
+    {
+        $ret = [
+            'flag' => 'FAIL',
+            'errMsg' => '上传失败，请稍后重试！'
+        ];
+        if ($request->is_post()) {
+            $imgDIR = "/a/mch/shop/";
+            $img = $_POST["img"];
+            $upload = new Upload($img, $imgDIR);
+            $upload->standardheight = 250;
+            $result = $upload->saveImgData();
+            $ret = $upload->buildUploadResult($result);
+        }
+        $response->sendJSON($ret);
+    }
 
-	}
+    /**
+     * 处理首页轮播图删除
+     * @param Request $request
+     * @param Response $response
+     */
+    public function carousel_del(Request $request, Response $response)
+    {
+
+    }
+
+    /**
+     * 添加首页轮播图
+     * @param Request $request
+     * @param Response $response
+     */
+    public function carousel_add(Request $request, Response $response)
+    {
+        /**
+         * 首先处理删除
+         * 1.得到用户的所有图片id
+         */
+        $ids = Shop_Model::getCarouselId();//得到carousel_id
+        $Arraydata = $request->post("imgArr");
+        if (empty($Arraydata)) {
+            $ret['retmsg'] = "不能删除所有轮播图！";
+            $ret['status'] = 0;
+            $response->sendJSON($ret);
+        }
+
+        foreach ($Arraydata as &$val) {
+            if ($val['carousel_id'] > 0 && !in_array($val['carousel_id'], $ids)) {
+                //删除轮播
+                Shop_Model::delCarouse($val['carousel_id']);
+            } elseif ($val['carousel_id'] == 0) {
+                    //处理新增
+
+                Shop_Model::Carouse();
+            }
+        }
+
+
+    }
 }
- 
+
 /*----- END FILE: Shop_Controller.php -----*/
--- merchants/shop/Shop_Model.php
+++ merchants/shop/Shop_Model.php
@@ -7,8 +7,40 @@
 defined('IN_SIMPHP') or die('Access Denied');
 
 class Shop_Model extends Model {
-	
-	
+
+    /**
+     * 查询用户所有的轮播图
+     */
+    static function selCarousel(){
+            $merchant_id=$GLOBALS['user']->uid;
+            $sql="select carousel_id,link_url,carousel_img,sort from shp_carousel where merchant_id = '%s'";
+            return D()->query($sql,$merchant_id)->fetch_array_all();
+    }
+
+    /**
+     * 得到carousel_id
+     */
+    static function getCarouselId(){
+        $merchant_id=$GLOBALS['user']->uid;
+        $sql="select carousel_id from shp_carousel where merchant_id = '%s'";
+        $carousel_ids = D()->query($sql,$merchant_id)->fetch_array_all();
+        $ids=[];
+        foreach($carousel_ids as $val){
+            array_push($ids,$val['carousel_id']);//转一
+        }
+            return $ids;
+    }
+
+    /**
+     * 删除轮播图
+     * @param $carousel_id
+     */
+    static function delCarouse($carousel_id){
+//        delete($tablename, $wherearr)
+            $tablename="`shp_carousel`";
+            $wherearr['carousel_id']=$carousel_id;
+            D()->delete($tablename,$wherearr);
+    }
 	
 }
  
