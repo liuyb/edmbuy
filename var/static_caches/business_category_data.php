f9302e1873c90612401106f6a8e154f3538022cf
lanzhengwu <lanzhengwu@foxmail.com>
2016-04-18 12:25:08 +0800

    优化DOM结构，去掉多余的ifr-body

---
+++ var/static_caches/business_category_data.php
@@ -0,0 +1,14 @@
+<?php
+$data = array (
+  0 => 
+  array (
+    'cat_id' => '2',
+    'cat_name' => '电器',
+  ),
+  1 => 
+  array (
+    'cat_id' => '1',
+    'cat_name' => '食品',
+  ),
+);
+?>
---
+++ var/static_caches/goods_type_data.php
@@ -0,0 +1,34 @@
+<?php
+$data = array (
+  0 => 
+  array (
+    'cat_id' => '1',
+    'cat_name' => '重量',
+    'enabled' => '1',
+    'attr_group' => '100g
+250g
+500g',
+  ),
+  1 => 
+  array (
+    'cat_id' => '2',
+    'cat_name' => '颜色',
+    'enabled' => '1',
+    'attr_group' => '\'\'',
+  ),
+  2 => 
+  array (
+    'cat_id' => '3',
+    'cat_name' => '尺寸',
+    'enabled' => '1',
+    'attr_group' => '\'\'',
+  ),
+  3 => 
+  array (
+    'cat_id' => '4',
+    'cat_name' => '款式',
+    'enabled' => '1',
+    'attr_group' => '\'\'',
+  ),
+);
+?>
